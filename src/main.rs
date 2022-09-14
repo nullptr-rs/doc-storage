e4e98d6a15070f83b55ba86bf32dee98e089ae40
Yggdrasil80 <louisdechorivit@gmail.com>
2022-09-14 15:53:24 +0200

    :ambulance: Try to fix main

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::env;
 use std::error::Error;
 use tokio::runtime::Builder;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> std::io::Result<()> {
     let worker_threads = env::var("WORKER_THREADS")
         .unwrap_or_else(|_| "8".to_string())
         .parse::<usize>()
         .unwrap();
+
+    print!("Starting server with {} worker threads...", worker_threads);
 
     System::with_tokio_rt(|| {
         Builder::new_multi_thread()
@@ -18,18 +20,17 @@ fn main() -> Result<(), Box<dyn Error>> {
             .build()
             .unwrap()
     })
-    .block_on(async_bootstrap())
+    .block_on(async_bootstrap(worker_threads))
 }
 
-async fn async_bootstrap() -> Result<(), Box<dyn Error>> {
-    print!("Hello, world!");
+async fn async_bootstrap(worker_threads: usize) -> std::io::Result<()> {
+    println!("done");
 
-    HttpServer::new(move || App::new().route("/", web::get().to(|| async { "Hello world!" })))
+    HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Hello world!" })))
+        .workers(worker_threads)
         .bind("0.0.0.0:8000")?
         .run()
-        .await?;
-
-    Ok(())
+        .await
     //https://lib.rs/crates/bita
     //https://discord.com/channels/648981252988338213/935847071540469820/1016443689679200286
     //TODO Add SHA256 checksum to chunks

7f64f296ec09e8970c780552541b7d04ed93f06d
Yggdrasil80 <louisdechorivit@gmail.com>
2022-09-14 15:45:28 +0200

    :ambulance: Try to fix Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use actix_web::rt::System;
-use actix_web::{App, HttpServer};
-use doc_storage::database::s3;
-use doc_storage::storage::chunk;
-use rusoto_s3::S3;
+use actix_web::{web, App, HttpServer, Responder};
 use std::env;
 use std::error::Error;
-use std::path::PathBuf;
-use std::time::SystemTime;
-use tokio::fs;
 use tokio::runtime::Builder;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -20,6 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Builder::new_multi_thread()
             .thread_name("doc-storage-worker")
             .worker_threads(worker_threads)
+            .enable_all()
             .build()
             .unwrap()
     })
@@ -29,13 +24,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 async fn async_bootstrap() -> Result<(), Box<dyn Error>> {
     print!("Hello, world!");
 
-    HttpServer::new(move || App::new())
-        .bind("127.0.0.1:1109")?
+    HttpServer::new(move || App::new().route("/", web::get().to(|| async { "Hello world!" })))
+        .bind("0.0.0.0:8000")?
         .run()
         .await?;
 
     Ok(())
     //https://lib.rs/crates/bita
     //https://discord.com/channels/648981252988338213/935847071540469820/1016443689679200286
-    //TODO Cache dependencies in Github CI and DockerFile, Add SHA256 checksum to chunks
+    //TODO Add SHA256 checksum to chunks
 }
